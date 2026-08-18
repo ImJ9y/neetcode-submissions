8ffe2302ddca937f6b3b51df2065580ed9f7d11d
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-08-18 16:04:43 -0700

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        closed_parenthese = {')':'(', '}':'{',']':'['}
+        res = []
+
+        for c in s:
+            if c in closed_parenthese:
+                if res and res[-1] == closed_parenthese[c]:
+                    res.pop()
+                    continue
+                else:
+                    return False
+            else:
+                res.append(c)
+        
+        return not res
